7e5a89c96cc3dd95f271cc401f5bd787224a111c
JorgeAndresMora8 <jamm08012002@gmail.com>
2025-01-22 10:38:01 -0400

    deployed11

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -27,7 +27,7 @@ PaymentRouter.get('/user/:id', async (req, res) => {
 PaymentRouter.post('/', async (req, res) => { 
     const { user, total, products, bank, installments } = req.body;
     await invoiceService.generateInvoce({user, total, products, bank, installments})
-    res.set('Access-Control-Allow-Origin', '*');
+    // res.set('Access-Control-Allow-Origin', '*');
     res.status(200).json({ state:'succesfull', message:'order recieved' })
 
 })
